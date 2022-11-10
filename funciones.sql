de6e5c6b1319a867fe9cc9875d321186b6e1a432
Gonzalo Martone <gmartone@itba.edu.ar>
2022-11-10 15:06:37 -0300

    LOOKS: cleanup

--- funciones.sql
+++ funciones.sql
@@ -1,40 +1,38 @@
 CREATE TABLE clientes_banco (
-    codigo   INT,
-    dni      INT NOT NULL, /*TODO ADD CHECKS > 0? MAKE UNIQUE?*/
-    telefono VARCHAR(100),
-    nombre   VARCHAR(100) NOT NULL,
-    direccion VARCHAR(100),
+    codigo      INT,
+    dni         INT NOT NULL,   /* TODO: ADD CHECKS > 0? MAKE UNIQUE?*/
+    telefono    VARCHAR(100),
+    nombre      VARCHAR(100) NOT NULL,
+    direccion   VARCHAR(100),
     PRIMARY KEY (codigo)
 );
-/*
-\copy clientes_banco(codigo, dni, telefono, nombre, direccion) FROM './clientes_banco.csv' DELIMITER ',' CSV HEADER;
-*/
+-- Load using `psql -h bd1.it.itba.edu.ar -U <user> PROOF`
+-- \copy clientes_banco(codigo, dni, telefono, nombre, direccion) FROM './clientes_banco.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE prestamos_banco (
-    codigo INT,
-    fecha DATE NOT NULL,
-    codigo_cliente INT NOT NULL,
-    importe INT NOT NULL,
+    codigo          INT,
+    fecha           DATE NOT NULL,
+    codigo_cliente  INT NOT NULL,
+    importe         INT NOT NULL,
     FOREIGN KEY (codigo_cliente) REFERENCES clientes_banco(codigo) ON DELETE CASCADE, /*ON UPDATE RESTRICT?*/
     PRIMARY KEY (codigo)
 );
 
-/*
-\copy prestamos_banco(codigo, fecha, codigo_cliente, importe) FROM './prestamos_banco.csv' DELIMITER ',' CSV HEADER;
-*/
+-- Load using `psql -h bd1.it.itba.edu.ar -U <user> PROOF`
+-- \copy prestamos_banco(codigo, fecha, codigo_cliente, importe) FROM './prestamos_banco.csv' DELIMITER ',' CSV HEADER;
+
 
 CREATE TABLE pagos_cuotas (
-    nro_cuota INT,        /* TODO: check if not null is necesary*/
-    codigo_prestamo INT, /* TODO: check if not null is necesary*/
-    importe INT NOT NULL, /* >0 ?*/
-    fecha DATE NOT NULL,
+    nro_cuota       INT,            /* TODO: check if not null is necesary*/
+    codigo_prestamo INT,            /* TODO: check if not null is necesary*/
+    importe         INT NOT NULL,   /* >0 ?*/
+    fecha           DATE NOT NULL,
     FOREIGN KEY (codigo_prestamo) REFERENCES prestamos_banco(codigo) ON DELETE CASCADE, /*ON UPDATE RESTRICT?*/
     PRIMARY KEY (codigo_prestamo, nro_cuota)
 );
 
-/*
-\copy pagos_cuotas(nro_cuota, codigo_prestamo, importe, fecha) FROM './pagos_cuotas.csv' DELIMITER ',' CSV HEADER;
-*/
+-- Load using `psql -h bd1.it.itba.edu.ar -U <user> PROOF`
+-- \copy pagos_cuotas(nro_cuota, codigo_prestamo, importe, fecha) FROM './pagos_cuotas.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE backup (
     dni                  INT,           -- dni cliente
